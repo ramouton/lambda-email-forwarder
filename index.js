5282e6ffc1e61d5298467169908c499c58212262
Raymond Mouton <raymond_mouton@icloud.com>
2017-06-24 18:47:19 -0500

    added SES send email support

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ exports.handler = (event, context, callback) => {
   var simpleParser = require('mailparser').simpleParser;
   
   var inbound_email_s3_bucket = process.env.INBOUND_EMAIL_BUCKET;
+  var outbound_source_address = process.env.SOURCE_EMAIL_ADDRESS;
   var outbound_forward_to_address = process.env.FORWARD_TO_EMAIL_ADDRESS;
   
   var email_message_id = event.Records[0].ses.mail.messageId;
@@ -24,24 +25,58 @@ exports.handler = (event, context, callback) => {
       console.log(err, err.stack);
       callback('s3 error',null);
     } else {
-      simpleParser(data.Body, (err, mail)=>{
+      simpleParser(data.Body, (err, inbound_email)=>{
         if (err) {
           console.log(err);
           callback('simpleParser error',null);
         } else {
-          console.log(mail);
-          callback(null,{"success":true});
+          if (mail.attachments.length == 0) {
+            var outbound_email_params = {
+              Destination: {
+                BccAddresses: [], 
+                CcAddresses: [], 
+                ToAddresses: [outbound_forward_to_address]
+              }, 
+              Message: {
+                Body: {
+                  Html: {
+                    Charset: "UTF-8", 
+                    Data: inbound_email.html 
+                  }, 
+                  Text: {
+                    Charset: "UTF-8", 
+                    Data: inbound_email.text 
+                  }
+                }, 
+                Subject: {
+                  Charset: "UTF-8", 
+                  Data: inbound_email.subject 
+                }
+              }, 
+              ReplyToAddresses: [inbound_email.from[0].address], 
+              Source: outbound_source_address 
+            };
+            ses.sendEmail(outbound_email_params, function(err, outbound_email) {
+              if (err) {
+                console.log(err, err.stack);
+              } else {
+                s3.deleteObject(params, function(err, kp_data) { 
+                  if (err) {
+                    console.log(err, err.stack);
+                    callback('s3 error',null);
+                  } else {
+                    callback(null,{"success":true});
+                  }
+                });
+              }
+            });
+          } else {
+            console.log('Emails with attachments not yet supported');
+            callback('Emails with attachments not yet supported',null);
+          }
         }
       });
     }
   });
-
-
-/*
-  var email_sender = event.Records[0].ses.mail.source;
-  var email_subject = event.Records[0].ses.mail.commonHeaders.subject;
-  var email_date = event.Records[0].ses.mail.commonHeaders.date;
-  console.log(email_sender,email_date,email_subject);
-*/
 
 };
