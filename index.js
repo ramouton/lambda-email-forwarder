50de0c5be8aabcefda51e86c5d18261f12c5a088
Raymond Mouton <raymond_mouton@icloud.com>
2017-06-24 14:35:15 -0500

    removing reserved word

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
-  var arguments = {};
-  arguments.event = event;
-  arguments.context = context;
-  console.log(arguments);
-  callback(null,arguments);
+  var inbound_args = {};
+  inbound_args.event = event;
+  inbound_args.context = context;
+  console.log(inbound_args);
+  callback(null,inbound_args);
 };
