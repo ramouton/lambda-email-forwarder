bbbfb7dba114de83a8ea52c77bf8eb012ba4336f
Raymond Mouton <raymond_mouton@icloud.com>
2017-06-24 17:20:27 -0500

    adding retrieval of raw email from s3

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 exports.handler = (event, context, callback) => {
   var AWS = require('aws-sdk');
+  var s3 = new AWS.S3();
+  var ses = new AWS.SES();
   
-  var email_sender = event.Records[0].ses.mail.source;
-  var email_subject = event.Records[0].ses.mail.commonHeaders.subject;
-  var email_date = event.Records[0].ses.mail.commonHeaders.date;
+  var inbound_email_s3_bucket = process.env.INBOUND_EMAIL_BUCKET;
+  var outbound_forward_to_address = process.env.FORWARD_TO_EMAIL_ADDRESS;
   
   var email_message_id = event.Records[0].ses.mail.messageId;
 
-  console.log('Need to put in AWS SES send mail call');
+  var s3_read_params = {
+    Bucket: inbound_email_s3_bucket,
+    Key: email_message_id
+  };
+  s3.getObject(s3_read_params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      callback({"success":false},null);
+    } else {
+      var rawEmail = data.Body;
+      console.log(rawEmail);
+
   
-  console.log(email_message_id);
+      callback(null,{"success":true});
+    }
+  });
+
+
+/*
+  var email_sender = event.Records[0].ses.mail.source;
+  var email_subject = event.Records[0].ses.mail.commonHeaders.subject;
+  var email_date = event.Records[0].ses.mail.commonHeaders.date;
   console.log(email_sender,email_date,email_subject);
-  
-  callback(null,{"success":true});
+*/
+
 };
