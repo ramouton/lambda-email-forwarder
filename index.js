c21daf0e0e0594aeb3ea621d9855dd79f62d0458
Raymond Mouton <raymond_mouton@icloud.com>
2017-06-24 15:19:41 -0500

    retrieving ses received email data.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 exports.handler = (event, context, callback) => {
-  var inbound_args = {};
-  inbound_args.event = event;
-  inbound_args.context = context;
-  console.log(inbound_args);
-  callback(null,inbound_args);
+  
+  var email_sender = event.Records[0].ses.mail.source;
+  var email_subject = event.Records[0].ses.mail.commonHeaders.subject;
+  var email_date = event.Records[0].ses.mail.commonHeaders.date;
+  
+  var email_message_id = event.Records[0].ses.mail.messageId;
+  
+  console.log(email_message_id);
+  console.log(email_sender,email_date,email_subject);
+  
+  callback(null,{"success":true});
 };
